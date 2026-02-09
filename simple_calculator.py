48e7f3893ae1ee97097d637612c219634429801e
daffa i <daffa2.ismail178@gmail.com>
2026-02-09 16:42:46 +0700

    changed add

--- simple_calculator.py
+++ simple_calculator.py
@@ -17,7 +17,7 @@ class Calculator:
 
 if __name__ == "main":
     calc = Calculator()
-    print("Addition:", calc.add(10,5))
+    print("Addition:", test6767676767676767677.add(10,5))
     print("Subtraction:", calc.subtract(10,5))
     print("Multiplication:", calc.multiply(10,5))
     print("Division:", calc.divide(10,5))
